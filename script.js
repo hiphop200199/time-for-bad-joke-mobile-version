5b50ecf41898a71a9ef08fa77b9ef5802423993b
Eric Tsai <hiphop200199@gmail.com>
2022-12-20 15:18:26 +0800

    調整版面

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ document.addEventListener("DOMContentLoaded",function(){
         let mouse = document.querySelector(".web-cursor");
         let webBody=document.body;
         let webTitle=document.querySelector(".web-title");
+        let pic=document.querySelector(".web-image");
         window.addEventListener("mousemove", cursor);
         randomBackgroundColor();
         randomTitleColor();
+        setInterval(changePic,1000);
+        setInterval(changePic2,2000);
+        setInterval(changePic3,3000);
         function cursor(e) {   
             mouse.style.top = e.pageY + "px"; 
             mouse.style.left = e.pageX + "px";
@@ -16,4 +20,13 @@ document.addEventListener("DOMContentLoaded",function(){
         function randomTitleColor(){
             webTitle.style.color=`rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
         };
-    });
+        function changePic(){
+                pic.src="./mypic2.png";   
+    };
+         function changePic2(){
+        pic.src="./mypic3.png";   
+};        
+        function changePic3(){
+    pic.src="./mypic.png";   
+};    
+});  

9790886a47929639db45922eddf04f3974ee7d4b
Eric Tsai <hiphop200199@gmail.com>
2022-12-28 02:00:26 +0800

    前台加音樂

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded",function(){
-      
+      let bgm=new Audio("cyberpunk.ogg");
+      let musicOn=document.querySelector(".bgm");
+      musicOn.addEventListener("click",function(){
+        if(bgm.paused){
+            bgm.volume=0.5;
+            bgm.play();
+        }else{
+        bgm.pause();
+        }
+      })
 });  

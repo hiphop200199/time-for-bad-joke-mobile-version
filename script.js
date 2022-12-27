adb71c441d0c913ba81b0d744bee6f65f1d972ad
Eric Tsai <hiphop200199@gmail.com>
2022-12-28 01:45:40 +0800

    漸漸加，前後端

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 document.addEventListener("DOMContentLoaded",function(){
-        let pic=document.querySelector(".web-image");
-        setInterval(changePic2,1000);
-        setInterval(changePic3,2000);
-         function changePic2(){
-        pic.src="./mypic3.png";   
-};        
-        function changePic3(){
-    pic.src="./mypic2.png";   
-};    
+      
 });  

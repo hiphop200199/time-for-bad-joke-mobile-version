0d311371c181db69956af3824f49548e17e3ff18
Eric Tsai <hiphop200199@gmail.com>
2022-12-28 00:12:54 +0800

    製作電腦、手機介面

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-
 document.addEventListener("DOMContentLoaded",function(){
-        let mouse = document.querySelector(".web-cursor");
         let pic=document.querySelector(".web-image");
-        window.addEventListener("mousemove", cursor);
-       
         setInterval(changePic2,1000);
         setInterval(changePic3,2000);
-        function cursor(e) {   
-            mouse.style.top = e.pageY + "px"; 
-            mouse.style.left = e.pageX + "px";
-        };
-       
-        
-          
-       
-      
-   
          function changePic2(){
         pic.src="./mypic3.png";   
 };        

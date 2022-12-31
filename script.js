15988cc0c1eed3d7780b5ad6090ab98e279954b9
Eric Tsai <hiphop200199@gmail.com>
2023-01-01 05:54:00 +0800

    調整拖拉模式

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 document.addEventListener("DOMContentLoaded",function(){
       let bgm=new Audio("cyberpunk.ogg");
       let soundEffect=new Audio("typing.ogg");
-      let musicOn=document.querySelector(".bgm");
+      let computer=document.querySelector(".computer");
       let startBtn=document.querySelector(".web-button");
-      musicOn.addEventListener("click",function(){
-        if(bgm.paused){
-            bgm.volume=0.5;
-            bgm.play();
-        }else{
-        bgm.pause();
-        }
-      })
+      let playstage=document.getElementById("playstage");
+      let offstage=document.getElementById("offstage");
+      let radio=document.getElementById("radio");
       startBtn.addEventListener("click",function(){
         
           soundEffect.play();
       })
-     
+      playstage.addEventListener("drop",dropAndPlay);
+      playstage.addEventListener("dragover",allowDrop);
+      offstage.addEventListener("drop",dropAndPause);
+      offstage.addEventListener("dragover",allowDrop);
+      radio.addEventListener("dragstart",drag);
+      function drag(event) {
+        event.dataTransfer.setData("text", event.target.id);
+      }
+      function dropAndPlay(event) {
+        event.preventDefault();
+        let data = event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(data));
+        document.getElementById(data).style.left = "1vmax";
+        document.getElementById(data).style.top = "1vmax";
+        bgm.autoplay=true;
+        bgm.loop=true;
+        bgm.load();
+        bgm.play();
+        computer.style.animation="beatsOhYeah 7s linear alternate infinite";
+       
+      }
+      function dropAndPause(event) {
+        event.preventDefault();
+        let data = event.dataTransfer.getData("text");
+        event.target.appendChild(document.getElementById(data));
+        document.getElementById(data).style.left = "1vmax";
+        document.getElementById(data).style.top = "1vmax";
+        bgm.pause();
+        computer.style.animation="none";
+      }
+      function allowDrop(event) {
+        event.preventDefault();
+      }
 });  

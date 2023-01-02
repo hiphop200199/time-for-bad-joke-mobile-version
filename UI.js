d80b68b57665c41affe29e30b378d24a4a95b2d2
Eric Tsai <hiphop200199@gmail.com>
2023-01-02 19:26:03 +0800

    字放大一點

--- UI.js
+++ UI.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",function(){
-      let jokes=["老鼠站不穩叫什麼?-->滑鼠","我知道大家都很想我，因為他們每天都會說中午要吃什麼...","i have a key,you have a value,oh, it`s Cookie!","你知道電腦怎麼句點人家嗎?他們分號人家...","你知道哪間科技公司最有邏輯嗎?羅技唷，因為他們是'logi''tech' ..."]; 
+      let jokes=["老鼠站不穩叫什麼?-->滑鼠","我知道大家都很想我，因為他們每天都會說中午要吃什麼...","i have a key,you have a value,oh, it`s Cookie!","你知道電腦怎麼句點人家嗎?他們分號人家...","你知道哪間科技公司最有邏輯嗎?羅技唷，因為他們是'logi''tech' ...","012345678...無nine..."]; 
       let bgm=new Audio("cyberpunk.ogg");
       let soundEffect=new Audio("typing.ogg");
       let computer=document.querySelector(".computer");
@@ -11,9 +11,8 @@ document.addEventListener("DOMContentLoaded",function(){
       startBtn.addEventListener("click",function(){
 
           soundEffect.play();
-          screenPage.innerHTML=jokes[Math.floor(Math.random()*4)];
-          
-          
+          screenPage.innerHTML=jokes[Math.floor(Math.random()*(jokes.length-1))];
+         
       })
       playstage.addEventListener("drop",dropAndPlay);
       playstage.addEventListener("dragover",allowDrop);
